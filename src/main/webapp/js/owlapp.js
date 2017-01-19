4d843fa2a621bae7914422d34c65ea3523073828
Luis <luis@luis.rains>
2017-01-19 15:46:53 -0300

    borre las pestañas y agrege un modal, mirar despacho como ejemplo

--- src/main/webapp/js/owlapp.js
+++ src/main/webapp/js/owlapp.js
@@ -84,6 +84,15 @@
                         "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
                         "sSortDescending": ": Activar para ordenar la columna de manera descendente"
                     }
+                },
+                columnDefs: [ {
+                    orderable: false,
+                    className: 'select-checkbox',
+                    targets:   0
+                } ],
+                select: {
+                    style:    'os',
+                    selector: 'td:first-child'
                 }
              });
 
