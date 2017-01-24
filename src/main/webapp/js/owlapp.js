af4ccf04b2ad0b55aaccf517e6187d109a04991f
Luis <luis@luis.rains>
2017-01-23 21:27:58 -0300

    crearDatatableEsp, importacion de datatable.select entre otros en starter

--- src/main/webapp/js/owlapp.js
+++ src/main/webapp/js/owlapp.js
@@ -129,8 +129,10 @@
 					 }
 					
 					 window.location.href = editUrl + id; //"/estudio/cliente/edit/" + id;
+					//codigo que selecciona la fila
+		            
 				 });
-				 $('#'+ dataTableId).css("width", "auto");
+				 
 		}
 		
 		
@@ -201,6 +203,8 @@
 			 //Ocultamos la primera columna (id)
 			 //dataTable.fnSetColumnVis(0, false);
              dataTable.column( 0 ).visible( false );
+             console.log("creando data:", dataTable)
+             //codigo que selecciona la fila
              $('#'+ dataTableId + ' tbody').on( 'click', 'tr', function () {
          		if ( $(this).hasClass('selected') ) {
          			$(this).removeClass('selected');
@@ -213,7 +217,7 @@
          			$("form input:last").val(data["id"]);
          		}
          	} );
-             $('#'+ dataTableId).css("width", "auto");
+             
            }
 		/**
 		 * 
@@ -246,83 +250,4 @@
 				});
 	    }*/
 		
-		function crearDataTableAuxiliar(dataTableId, ajaxSource, columnas){
-			console.log("creando DT:", dataTableId, ajaxSource, columnas)
-			
-			 /*var config={};
-			 config['processing'] = true;
-			 config['sAjaxSource'] = ajaxSource;
-			 config['serverSide'] = true;
-			 config['columns']= getColumnasArray(columnas);*/
-             var LENGUAJE = {
-                    "sProcessing": "Procesando...",
-                    "sLengthMenu": "Mostrar _MENU_ registros",
-                    "sZeroRecords": "No se encontraron resultados",
-                    "sEmptyTable": "Ningún dato disponible en esta tabla",
-                    "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                    "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-                    "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
-                    "sInfoPostFix": "",
-                    "sSearch": "Buscar:",
-                    "sUrl": "",
-                    "sInfoThousands": ",",
-                    "sLoadingRecords": "Cargando...",
-                    "oPaginate": {
-                        "sFirst": "Primero",
-                        "sLast": "Último",
-                        "sNext": "Siguiente",
-                        "sPrevious": "Anterior"
-                    },
-                    "oAria": {
-                        "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-                        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                    }
-                }
-			console.log("creando lenguaje:", LENGUAJE)
-			 //var dataTable = $('#'+ dataTableId).dataTable(config);
-			 var dataTable = $('#'+ dataTableId).dataTable({
-                'processing' : true,
-                'sAjaxSource' : ajaxSource,
-                'serverSide' : true,
-                'columns' : getColumnasArray(columnas),
-                'language' : {
-                    "sProcessing": "Procesando...",
-                    "sLengthMenu": "Mostrar _MENU_ registros",
-                    "sZeroRecords": "No se encontraron resultados",
-                    "sEmptyTable": "Ningún dato disponible en esta tabla",
-                    "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                    "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-                    "sInfoFiltered": "\n(filtrado de un total de _MAX_ registros)",
-                    "sInfoPostFix": "",
-                    "sSearch": "Buscar:",
-                    "sUrl": "",
-                    "sInfoThousands": ",",
-                    "sLoadingRecords": "Cargando...",
-                    "oPaginate": {
-                        "sFirst": "Primero",
-                        "sLast": "Último",
-                        "sNext": "Siguiente",
-                        "sPrevious": "Anterior"
-                    },
-                    "oAria": {
-                        "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-                        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                    }
-                }
-             });
-			console.log("creando data:", dataTable)
-			 //Ocultamos la primera columna (id)
-			 dataTable.fnSetColumnVis(0, false);
-			 
-				 $('#'+ dataTableId + ' tbody').on('click', 'tr', function() {
-					 //Obtenemos el valor de la columna id
-					 var id = dataTable.fnGetData(this, 0);
-					 //Si id no es número, no hacemos nada
-					 if (isNaN(id)) {
-					 return;
-					 }
-					
-					 window.location.href = editUrl + id; //"/estudio/cliente/edit/" + id;
-				 });
-			
-		}
+		
