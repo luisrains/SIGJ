b8a135656f78749e9d7160b5622ddb6ae5968028
Luis <luis@luis.rains>
2017-02-11 10:55:34 -0300

    puse en comentario algunos plugins de datatable en el starter.html para que se pueda ocultar el ID, poner el <th>Accion</th> a sus columnas del datatable en el index.html de sus clases

--- src/main/webapp/js/owlapp.js
+++ src/main/webapp/js/owlapp.js
@@ -15,14 +15,23 @@
 					//3 , se puede crear una funcion aparte en vez de hacer function
 					jQuery.each(arrayCol, function(i, val){
 						
-								if(val==true){val = "Si";}else if(val == false){val="No";}
-								columnsArray.push( {"data" : val} );
+								 if(val==true){
+									val = "Si";
+								}else if(val == false){
+									val="No";
+									
+								}else{
+									columnsArray.push( {"data" : val} );
+								}
+								console.log(val);
+								
 					});
 					columnsArray.push(
 			                {'defaultContent': "<button type='button' class='editar btn btn-primary btn-xs'><i class='fa fa-edit'></i></button>"+
 						                    "<button type='button' class='eliminar btn btn-danger btn-xs' data-toggle='modal'><i class='fa fa-trash-o' aria-hidden='true'></i></button>"+
 						                    "<button type='button' class='ver btn btn-primary btn-xs' ><i class='fa fa-search'></i></button>"}
 			                );
+					
 					console.log(colsStr, "->", columnsArray);
 					
 					return columnsArray;
@@ -57,35 +66,7 @@
 		function crearDataTable(dataTableId, ajaxSource, columnas, editUrl){
 			console.log("creando DT:", dataTableId, ajaxSource, columnas, editUrl)
 			
-			 /*var config={};
-			 config['processing'] = true;
-			 config['sAjaxSource'] = ajaxSource;
-			 config['serverSide'] = true;
-			 config['columns']= getColumnasArray(columnas);*/
-             var LENGUAJE = {
-                    "sProcessing": "Procesando...",
-                    "sLengthMenu": "Mostrar _MENU_ registros",
-                    "sZeroRecords": "No se encontraron resultados",
-                    "sEmptyTable": "Ningún dato disponible en esta tabla",
-                    "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                    "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-                    "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
-                    "sInfoPostFix": "",
-                    "sSearch": "Buscar:",
-                    "sUrl": "",
-                    "sInfoThousands": ",",
-                    "sLoadingRecords": "Cargando...",
-                    "oPaginate": {
-                        "sFirst": "Primero",
-                        "sLast": "Último",
-                        "sNext": "Siguiente",
-                        "sPrevious": "Anterior"
-                    },
-                    "oAria": {
-                        "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-                        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                    }
-                }
+			
 			 //var dataTable = $('#'+ dataTableId).dataTable(config);
 			 var dataTable = $('#'+ dataTableId).DataTable({
                 'processing' : true,
@@ -93,14 +74,15 @@
                 'sAjaxSource' : ajaxSource,
                 'serverSide' : true,
                 'columns' : getColumnasArray(columnas),
+
                 'language' : {
                     "sProcessing": "Procesando...",
                     "sLengthMenu": "Mostrar _MENU_ registros",
                     "sZeroRecords": "No se encontraron resultados",
-                    "sEmptyTable": "Ningún dato disponible en esta tabla",
+                    "sEmptyTable": "Ningun dato disponible en esta tabla",
                     "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                    "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-                    "sInfoFiltered": "\n(filtrado de un total de _MAX_ registros)",
+                    "sInfoEmpty": "Mostrando _MAX_ registros",
+                    "sInfoFiltered": " ",
                     "sInfoPostFix": "",
                     "sSearch": "Buscar:",
                     "sUrl": "",
@@ -117,11 +99,11 @@
                         "sSortDescending": ": Activar para ordenar la columna de manera descendente"
                     }
                 }
-             });
+			 });
 
 			 //Ocultamos la primera columna (id)
 			 //dataTable.fnSetColumnVis(0, false);
-			dataTable.column( 0 ).visible( false );
+			dataTable.column( 0 ).visible( false );		
 			
 				$('#'+ dataTableId + ' tbody').on('click','button.editar', function(){
 					/*Con esto tenemos un objeto de tipo {id: 1, codigo: "ejc", descripcion: "Ejecutivo", materia: Object}
