0acfabbb057aec02697e65ebc0599ca341c16e8e
ariquelmc <analiariquelme_93@hotmail.com>
2017-04-21 14:50:52 -0400

    Ingreso Egreso terminado.

--- src/main/java/py/com/sigj/gastos/controllers/form/IngresoEgresoFormController.java
+++ src/main/java/py/com/sigj/gastos/controllers/form/IngresoEgresoFormController.java
@@ -6,13 +6,16 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import py.com.sigj.controllers.form.FormController;
 import py.com.sigj.dao.Dao;
+import py.com.sigj.gastos.domain.IngresoEgreso;
 import py.com.sigj.gastos.controllers.list.IngresoEgresoListController;
 import py.com.sigj.gastos.dao.IngresoEgresoDao;
 import py.com.sigj.gastos.domain.IngresoEgreso;
@@ -62,18 +65,82 @@ public class IngresoEgresoFormController extends FormController<IngresoEgreso> {
 	}
 
 	@RequestMapping(value = "accion2", method = RequestMethod.POST)
-	public String accion2(ModelMap map, @Valid IngresoEgreso obj, @RequestParam(required = false) String accion,
+	public String accion2(ModelMap map, @Valid IngresoEgreso obj, BindingResult bindingResult,
+			@RequestParam(required = false) String accion,
 			@RequestParam(value = "id_objeto", required = false) Long id_objeto) {
 		if (StringUtils.equals(accion, "save")) {
-			//return guardar_listado(map, obj);
+			return guardar_listado(map, obj, bindingResult);
 		} else if (StringUtils.equals(accion, "edit")) {
-			logger.info("OBJETO INGRESO_EGRESO {}", obj);
-			//return editar_listado(map, obj);
+			logger.info("OBJETO Ingreso Egreso {}", obj);
+			return editar_listado(map, obj, bindingResult);
 		} else if (id_objeto != null) {
-			//return eliminar_listado(map, id_objeto);
+			return eliminar_listado(map, id_objeto);
+
 		}
 		return getTemplatePath();
 
 	}
-}
+
 	
+	@RequestMapping(value = "save_listado", method = RequestMethod.POST)
+	public String guardar_listado(ModelMap map, @Valid IngresoEgreso obj, BindingResult bindingResult) {
+		try {
+			if (obj.getId() == null) {
+				getDao().createOrUpdate(obj);
+
+				map.addAttribute("msgExito", msg.get("Registro agregado"));
+				logger.info("Se crea un nuevo Ingreso Egreso -> {}", obj);
+			}
+
+		} catch (Exception ex) {
+			obj.setId(null);
+			map.addAttribute("error", getErrorFromException(ex));
+		}
+		map.addAttribute(getNombreObjeto(), obj);
+		agregarValoresAdicionales(map);
+		return getTemplatePath();
+
+	}
+
+	@RequestMapping(value = "editar_listado", method = RequestMethod.POST)
+	public String editar_listado(ModelMap map, @Valid IngresoEgreso obj, BindingResult bindingResult) {
+		try {
+			if (obj != null) {
+				getDao().createOrUpdate(obj);
+				logger.info("Estado Externo Interno Actualizado {}", obj);
+				map.addAttribute("msgExito", msg.get("Registro Actualizado"));
+			}
+		} catch (Exception ex) {
+			obj.setId(null);
+			map.addAttribute("error", getErrorFromException(ex));
+			map.addAttribute(getNombreObjeto(), obj);
+		}
+		agregarValoresAdicionales(map);
+		return getTemplatePath();
+	}
+
+	@RequestMapping(value = "eliminar_listado", method = RequestMethod.POST)
+	public String eliminar_listado(ModelMap map, @RequestParam("id_objeto") Long id_objeto) {
+		
+		IngresoEgreso estado = null;
+		try {
+			logger.info("ID DE OBJ {}", id_objeto);
+			if (id_objeto != null) {
+				estado = getDao().find(id_objeto);
+				
+				getDao().destroy(estado);
+
+				logger.info("Estado Externo Interno eliminado  {}", estado);
+				map.addAttribute("msgExito", msg.get("Registro Eliminado"));
+			}
+		} catch (UnexpectedRollbackException ex) {
+			estado.setId(null);
+			map.addAttribute("error", "No puede borrar el Objeto");
+			map.addAttribute(getNombreObjeto(), estado);
+		}
+		map.addAttribute(getNombreObjeto(), getNuevaInstancia());
+		agregarValoresAdicionales(map);
+		return getTemplatePath();
+
+	}
+}
