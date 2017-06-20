0428d4d9086d685aae0819cab3dd1cd735f27529
Misael Villalba <victormisael7@gmail.com>
2017-06-19 22:54:25 -0400

    Se agrega select en movimiento opcional para cargar expedientes relacionados

--- src/main/java/py/com/sigj/rrhh/dao/MovimientoDao.java
+++ src/main/java/py/com/sigj/rrhh/dao/MovimientoDao.java
@@ -3,6 +3,7 @@ package py.com.sigj.rrhh.dao;
 import py.com.sigj.dao.Dao;
 import py.com.sigj.rrhh.domain.Movimiento;
 
+
 public interface MovimientoDao extends Dao<Movimiento> {
 
 }
---
+++ src/main/java/py/com/sigj/rrhh/dao/MovimientoExpedienteDao.java
@@ -0,0 +1,8 @@
+package py.com.sigj.rrhh.dao;
+
+import py.com.sigj.dao.Dao;
+import py.com.sigj.rrhh.domain.MovimientoExpediente;
+
+public interface MovimientoExpedienteDao extends Dao<MovimientoExpediente> {
+
+}
